37beb9c2104a8908087a405978c63411ab15bbc2
Ian Hampson <inhampson@gmail.com>
2016-03-08 09:30:29 -0500

    Monday changes wrapped.

--- client/client.js
+++ client/client.js
@@ -1,7 +1,10 @@
 var socket;
+var connect;
+var send;
+var host;
 
-/// Connects to the socketIo lib
-function setupSocket(){
+/// Connects to the socketIo lib and establishes responses
+var connectSocket = () => {
   var username = document.querySelector('#userName').value;
   if(!username){
     alert("You need to input a username");
@@ -11,29 +14,121 @@ function setupSocket(){
     socket = io.connect();
     socket.emit('join', {name:username});
 
-    socket.on('new', function(data){
+    socket.on('new', data => {
       console.log(data);
-      // if(data.name){
-      // var newCan = document.createElement(canvas);
-      // }
+      if(data.name){
+        var newUser = document.createElement('div');
+
+        var newLabel = document.createElement('span');
+        newLabel.innerHTML = data.name;
+
+        var newCan = document.createElement('canvas');
+        newCan.setAttribute('id',data.name);
+        newCan.setAttribute('sId', data.sId);
+        newCan.setAttribute('width', '40%');
+        newCan.setAttribute('height', '40%');
+
+        newUser.appendChild(newLabel);
+        newUser.appendChild(newCan);
+        document.body.appendChild(newUser);
+      }
     });
-    socket.on('updateClient', function(data){
+
+    socket.on('remove', data => {
+      var tar = document.querySelector(`#${data.name}`);
+      var container = tar.parentNode;
+      document.body.removeChild(container);
+    });
+
+    socket.on('updateClient', data => {
       // grab the right canvas and update it... somehow
     });
 
-    socket.on('clearPara',function(data){
+    socket.on('clearPara', data => {
       console.log("Clear something maybe...");
     });
   }
-}
 
-function init(){
-  var connect = document.querySelector("#join");
-  connect.addEventListener('click',setupSocket);
-}
+  window.onbeforeunload = _ =>{
+    socket.emit('leaving', {name:username});
+  };
 
-function sendMessage(){
+  connect.style.display = 'none';
+  send.style.display = 'inline';
+};
+
+/// Sends the message to the server
+var sendMessage = () => {
   socket.emit('msg', {data:"stuff"});
-}
+};
+
+var paintLocalSetup = () => { // Code from William Malone at http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
+  var hostContext = host.getContext('2d');
+  var paint;
+
+  var clickX = [];
+  var clickY = [];
+  var clickDrag = [];
+
+  host.addEventListener('mousedown', e => {
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+
+    paint = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
+  });
+
+  host.addEventListener('mousemove', e => {
+    if(paint){
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+      redraw();
+    }
+  });
+
+  host.addEventListener('mouseup', e => {
+    paint = false;
+  });
+
+  host.addEventListener('mouseleave', e => {
+    paint = false;
+  });
+
+  var addClick = (x, y, dragging) => {
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+  };
+
+  var redraw = () => {
+    hostContext.clearRect(0, 0, hostContext.canvas.width, hostContext.canvas.height); // Clears the canvas
+    hostContext.strokeStyle = "#3500E3";
+    hostContext.lineJoin = "round";
+    hostContext.lineWidth = 5;
+
+    for(var i=0; i < clickX.length; i++) {
+      hostContext.beginPath();
+      if(clickDrag[i] && i){
+        hostContext.moveTo(clickX[i-1], clickY[i-1]);
+      }else{
+        hostContext.moveTo(clickX[i]-1, clickY[i]);
+      }
+      hostContext.lineTo(clickX[i], clickY[i]);
+      hostContext.closePath();
+      hostContext.stroke();
+    }
+  };
+
+};
+
+var init = () => {
+  connect = document.querySelector("#join");
+  send = document.querySelector("#send");
+  host = document.querySelector("#host");
+  connect.addEventListener('click',connectSocket);
+  send.addEventListener('click',sendMessage);
+  paintLocalSetup();
+};
+
 
 window.onload=init;
--- server/server.js
+++ server/server.js
@@ -10,52 +10,51 @@ var client = fs.readFileSync(__dirname + "/../client/client.html");
 server.listen(port);
 console.log(`Listening on port:${port}`);
 
-const init = () => {
-  var roomCount = 0;
+var init = () => {
   var cCount = 0;
   var users = { };
 
   var OnJoin = socket => {
-    socket.on('join',function(data){
+    socket.on('join',data => {
       cCount += 1;
-      if(roomCount <5){
-        io.sockets.in('room1').emit('new',{name:data.name});
+        io.sockets.in('room1').emit('new',{name:data.name, sId:socket.id});
         socket.join('room1');
         console.log("joined room1");
         if(cCount !== 1){
-          console.log('Sending previously connected users');
           for(var key in users){
             console.log(users[key]);
-            socket.emit('new', key);
+            socket.emit('new', users[key]);
           }
-          console.log('Users sent');
         }
         users[data.name] = {name:data.name, sId:socket.id};
-        roomCount += 1;
-      } else{
-        socket.join('overflow');
-        console.log('room1 overflowed');
-      }
+    });
+
+    socket.on('leaving', data => {
+      socket.broadcast.emit('remove',{name : data.name});
+      delete users[data.name];
+      socket.leave('room1');
     });
   };
 
   var OnMsg = socket => {
-
+    socket.on('msg',data => {
+      socket.broadcast.emit('response',data);
+    });
   };
 
-  app.get('/', function(req, res){
+  app.get('/', (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write(client);
     res.end();
   });
 
-  app.get('/client/client.js', function(req,res){
+  app.get('/client/client.js', (req,res) => {
     res.writeHead(200, {"Content-Type": "application/javascript"});
     res.write(fs.readFileSync(__dirname+"/../client/client.js"));
     res.end();
   });
 
-  io.sockets.on("connection",function(socket){
+  io.sockets.on("connection", socket => {
     OnJoin(socket);
     OnMsg(socket);
   });
